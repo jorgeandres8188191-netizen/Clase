84fac7683d150d842f8b8f816dc0e8c412ec69a1
jorgeandres8188191-netizen <jorge.andres8188191@gmail.com>
2025-08-21 09:34:25 -0500

    Segundo

--- class/Program.cs
+++ class/Program.cs
@@ -1,2 +1,57 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Data.Common;
+
 Console.WriteLine("Hello, World!");
+
+Motos moto = new Motos();
+
+moto.id = 1;
+moto.Marca = new Marcas();
+moto.Modelo = "Szrr";
+moto.Año = 2025;
+moto.Cilindraje= 149.6f;
+moto.Color = new Colores();
+
+
+
+
+public class Colores
+{
+
+ public int Id;
+ public string? Nombre { get ;  set; } 
+
+}
+
+public class Marcas
+{
+    
+     public int Id;
+ public string? Nombre { get ;  set; } 
+
+}
+public class Motos
+{
+
+    public int id;
+    public Marcas? Marca { get ; set; } 
+    public string? Modelo { get ; set; } 
+    public bool? Encendida { get ; private set; } 
+    public float? Cilindraje { get ; set; } 
+    public Colores? Color { get ; set; }
+    public int? Año { get ; set; }  
+
+
+
+    public Motos()
+    {
+    }
+}
+
+public class Deportivas : Motos
+{
+
+    public float? VelocidadMaxima { get ; private set; } 
+
+}
+
